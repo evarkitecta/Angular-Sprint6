1e513707197a3f6b8ac21dfda413ab5516ca8a43
evarkitecta <evarkitecta@gmail.com>
2023-07-14 13:58:54 +0200

    N1 Exercici 4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -7,9 +7,23 @@ import { Phrase } from '../home/interfaces/phrase.interface';
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
+
   //Con @Input le indicamos que el atributo phrasesEscena recibirá datos de otros componentes
   @Input()
-  public phrasesEscena: Phrase[] = [
+  public phrasesEscena: Phrase[] = [];
+  public currentSentence: number = 0;
 
-  ];
+  prev() {
+    this.currentSentence--;
+    if (this.currentSentence < 0) {
+      this.currentSentence = this.phrasesEscena.length - 1;
+    }
+  }
+
+  next() {
+    this.currentSentence++;
+    if (this.currentSentence >= this.phrasesEscena.length) {
+      this.currentSentence = 0;
+    }
+  }
 }
